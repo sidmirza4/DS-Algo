60932fc0752aed7fadfe5a0984a33b9e443c1f38
Sid <sidmirza4@gmail.com>
2021-04-15 16:57:39 +0530

    singlyLinkedList complete

--- singlyLinkedList.js
+++ singlyLinkedList.js
@@ -67,6 +67,81 @@ class SinglyLinkedList {
 		this.length++;
 		return this;
 	}
+
+	get(index) {
+		if (index < 0 || index >= this.length) return null;
+		if (index === 0) return this.head;
+		let counter = 0;
+		let current = this.head;
+		while (counter !== index) {
+			current = current.next;
+			counter++;
+		}
+
+		return current;
+	}
+
+	set(index, val) {
+		let current = this.get(index);
+		if (current) {
+			current.val = val;
+			return true;
+		}
+		return false;
+	}
+
+	insert(index, val) {
+		if (index < 0 || index > this.length) return false;
+		if (index === this.length) return !!this.push(val);
+		if (index === 0) return !!this.unshift(val);
+
+		const newNode = new Node(val);
+		const prev = this.get(index - 1);
+		let temp = prev.next;
+		prev.next = newNode;
+		newNode.next = temp;
+		this.length++;
+
+		return true;
+	}
+
+	remove(index) {
+		if (index < 0 || index >= this.length) return null;
+		if (index === this.length - 1) return this.pop();
+		if (index === 0) return this.shift();
+
+		const prev = this.get(index - 1);
+		const removedNode = prev.next;
+		prev.next = removedNode.next;
+		this.length--;
+		return removedNode;
+	}
+
+	print() {
+		let arr = [];
+		let current = this.head;
+		while (current) {
+			arr.push(current.val);
+			current = current.next;
+		}
+		console.log(arr);
+	}
+
+	reverse() {
+		let node = this.head;
+		this.head = this.tail;
+		this.tail = node;
+		let prev = null;
+		let next;
+		for (let i = 0; i < this.length; i++) {
+			next = node.next;
+			node.next = prev;
+			prev = node;
+			node = next;
+		}
+
+		return this;
+	}
 }
 
 // var first = new Node("Hi")
@@ -81,7 +156,10 @@ var list = new SinglyLinkedList();
 // list.push('How are you?');
 list.push(99);
 list.push('How are you?');
+list.insert(2, 'are you doing greate?');
 // list.shift();
 // list.shift();
 
+// console.log(list.set(0, 'edited'));
+// console.log(list);
 console.log(list);
